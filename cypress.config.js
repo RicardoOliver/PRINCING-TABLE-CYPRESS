9e0b9181b82ca6af0a26639f7afebbc822ad75e8
Ricardo <r.c.d.1985@hotmail.com>
2023-02-13 22:17:20 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:5500',
+    baseUrl: 'https://ricardooliver.github.io',
     experimentalRunAllSpecs: true,
     viewportWidth: 1024,
     viewportHeight: 768,
